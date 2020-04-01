c722289577a3089ccf0d68b04730353f91d0467c
Steffbeard <galacticraft.biz@gmail.com>
2020-04-01 09:07:00 -0400

    added skulls

--- src/main/java/com/steffbeard/totalwar/modules/tweaks/listeners/AnimalDropsListener.java
+++ src/main/java/com/steffbeard/totalwar/modules/tweaks/listeners/AnimalDropsListener.java
@@ -1,23 +1,28 @@
 package com.steffbeard.totalwar.modules.tweaks.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Cow;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Llama;
 import org.bukkit.entity.Pig;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 /**
  * 
- * Handles custom mob drops from killing animals
- * Makes getting bones easier and chance for other stuff like spider eyes
+ * Handles custom mob drops from killing animals.
+ * Makes getting bones easier and chance for other stuff like spider eyes.
+ * 
  * 
  * @author Steffbeard
  *
@@ -32,6 +37,7 @@ public class AnimalDropsListener implements Listener {
 	 * 
 	 * @param event
 	 */
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void animalDrops(EntityDeathEvent event) {
 		LivingEntity e = event.getEntity();
@@ -39,30 +45,28 @@ public class AnimalDropsListener implements Listener {
 		
 		if(e instanceof Pig) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Cow) {
+		} else if(e instanceof Cow) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Sheep) {
+		} else if(e instanceof Sheep) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Horse) {
+		} else if(e instanceof Horse) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Wolf) {
+		} else if(e instanceof Wolf) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Llama) {
+		} else if(e instanceof Llama) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
-		}
-		
-		if(e instanceof Chicken) {
+		} else if(e instanceof Chicken) {
 			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
+		} else if(e instanceof Player) {
+			// Got to define what a skull is
+			ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+			SkullMeta meta = (SkullMeta) skull.getItemMeta();
+			meta.setOwner(e.getName());
+			meta.setDisplayName(ChatColor.YELLOW + e.getName() + "'s Skull");
+			skull.setItemMeta(meta);
+			
+			e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.BONE));
+			e.getLocation().getWorld().dropItem(e.getLocation(), skull);
 		}
 	}
 }
